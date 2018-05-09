73dea1cb047d480f69440535c291234603072344
FashionTech <38173796+FashionTech@users.noreply.github.com>
2018-05-09 17:10:10 -0300

    Add files via upload

---
+++ buzzer/buzzer.ino
@@ -0,0 +1,19 @@
+void setup(){
+  pinMode(10,OUTPUT); //define o pin 13 do Arduino como saída digital
+}
+void loop(){
+//play tone
+  tone(10,1760,1000);
+  delay(1000);
+  tone(10,1975,1000);
+  delay(1000);
+  tone(10,1175,1000);
+  delay(1000);
+  tone(10,1315,1000);
+  delay(1000);
+  tone(10,2795,1000);
+  delay(1000);
+  tone(10,1175,1000);
+  delay(1000);
+}
+
